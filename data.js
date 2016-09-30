24dc290e07dde9b595db712b48656f8957e85f32
Elena Quevedo <elenaquevedo@Elenas-MacBook-Pro.local>
2016-09-29 18:19:21 -0700

    Pike Place done

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //Here's what Jo wants on the data page: for each location, a list that looks exactly like this example:
 ///Pike Place Market
 ////6:00am: 86.4 lbs [23 customers, 27.6 cups (1.4 lbs), 85 lbs to-go]
@@ -11,34 +13,50 @@
 ///Total to-go pound packages sold at Pike Place Market: 26
 ///Total pounds of beans needed at Pike Place Market: 38.4//
 
-var PikePlace = {
-  min: 14,
-  max: 35,
-  cups: 1.2,
-  totalBeans: 0.34,
-  customersPerHour: [],
-  hours: ['6am','7am','8am','9am','10am','11am',
-          '12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm'],
-
 //random number of customers//
-  getRandom: function(min,max) {
-    var num = Math.floor(Math.random() * ( max - min + 1)) + min;
-    customersPerHour.push(num);
-  }
-};
+  getRandomCustomer: function(min,max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  },
   //calculateCustomersPerHour
   //Total customers = 235 (# of customers* 16 hours open)
   //(work backwards) getRandom: function(min,max) {
   //  return (Math.floor(Math.random() * ( max - min ) + min) );
 
+  generateCustomerData: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      this.custPerHour.push(this.getRandomCustomer(this.minCust, this.maxCust));
+      this.custPerDay += this.custPerHour[i];
+    }
+  },
+
 //cups/hr/loc
   //for (var i=0; i<hours.length; i++) {
   //  return [i];
-  //}
+  //
+
+  generateCupsData: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      this.cupsPerHour.push(this.custPerHour[i] * this.averageCups);
+      this.cupsPerDay += this.cupsPerHour[i];
+    }
+  },
+
+  generateEmployeeData: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      this.employeesPerHour.push(Math.ceil(this.custPerHour[i] / 30));
+      this.employeesPerDay += this.employeesPerHour[i];
+    }
+  },
 
 //total amount of beans 1lb = 16cups; per hr/daily/loc total then company total
 //Total pounds of beans 38.4 Pike place
 //totalBeans/hour * 15 hours open per location + total for each location
+  generateLbsData: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      this.lbsPerHour.push(this.custPerHour[i] * this.averagePounds);
+      this.lbsPerDay += this.lbsPerHour[i];
+    }
+  },
 
 //message += <p>'hours[i]' + ':' + totalPounds +'[PikePlace[customers], PikePlace[cups],PikePlace[pounds],PikePlace[to-go]]'</p>;
 
@@ -46,5 +64,30 @@ var PikePlace = {
 //employees per hour each location each hour//each customer=2minutes//Math.floor
 // 60minutes an hour /2minutes = 30 customers served/hr * 15 hours = 450
 
+};
 //Total cups = 189
 //(work backwards) cups/hr * 15 (no of hours open)
+
+pikePlaceMarket.generateCustomerData();
+pikePlaceMarket.generateCupsData();
+pikePlaceMarket.generateLbsData();
+pikePlaceMarket.generateEmployeeData();
+
+///////////Capitol Hill/////////////
+////////////////////////////////////
+
+var capitolHill = {
+  location: 'Capitol Hill',
+  minCust: 14,
+  maxCust: 35,
+  averageCups: 1.2,
+  averagePounds: .34,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
+  custPerHour: [],
+  custPerDay: 0,
+  cupsPerHour: [],
+  cupsPerDay: 0,
+  lbsPerHour: [],
+  lbsPerDay: 0,
+  employeesPerHour: [],
+  employeesPerDay: 0,
